5812a889f6660f1b1d534b666046d1eaa98aad74
foree <foreexistsd@gmail.com>
2023-09-30 16:39:35 +0300

    gyh

--- run.py
+++ run.py
@@ -40,8 +40,7 @@ def run_solver(inst, algo, default='-'):
     solver.type = 'U1S'
     solution.set_rewards(solver, inst)
     res = tuple(zip(solution.rewards, timestamps))
-    fin_res = solution.rewards[-1]
-    time = timestamps[-1] if not solution.interrupted else default
+    time, fin_res = timestamps[-1], solution.rewards[-1] if not solution.interrupted else default, default
     return fin_res, time, res, states
 
 
