ab38155b7c082190576ed766e86aee0f0ae9e50e
Ev713 <foreexistsd@gmail.com>
2023-11-21 13:05:27 +0200

    Fixed heuristic

--- all_run.sh
+++ all_run.sh
@@ -17,16 +17,11 @@ fi
 
 algos=('BFS' 'BNB' 'BNBL' 'MCTS_V' 'MCTS_E' 'GBFS' )
 
-num_algos=${#algos[@]}
-cpu_limit_percent=80
-cpu_limit_per_process=$(echo "scale=2; $cpu_limit_percent / ($end_index-$start_index + 1) / $num_algos" | bc)
 
 # Loop through processes and strategies
 for i in $(seq $start_index $end_index); do
     for algo in "${algos[@]}"; do
         python3 run.py "$i" "$algo"  &
-        pid=$!
-        cpulimit --pid=$pid --limit=$cpu_limit_per_process &
     done
 done
 
